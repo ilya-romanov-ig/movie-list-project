66336eac3464917f8c9d5fad27c24858379f076d
root <root@DESKTOP-82M5BL7.localdomain>
2025-11-29 00:28:05 +0300

    front

--- front/src/plugins/vuetify.js
+++ front/src/plugins/vuetify.js
@@ -3,13 +3,13 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
-const myCustomLightTheme = {
-  dark: false,
-  colors: {
-    primary: '#1867C0',
-    secondary: '#5CBBF6',
-  }
-}
+// const myCustomLightTheme = {
+//   dark: false,
+//   colors: {
+//     primary: '#1867C0',
+//     secondary: '#5CBBF6',
+//   }
+// }
 
 const vuetify = createVuetify({
     components,
